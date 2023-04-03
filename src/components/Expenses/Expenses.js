0fa56984174d7314afc13145268fd67850706571
Glenn <glennmeekers@gmail.com>
2023-04-02 17:31:26 -0700

    moved expenses list to its own component file and added a new fallback method

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
@@ -17,19 +17,6 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === enteredYear;
   }
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((filteredExpense) => (
-      <ExpenseItem
-        key={filteredExpense.id}
-        title={filteredExpense.title}
-        amount={filteredExpense.amount}
-        date={filteredExpense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -37,8 +24,7 @@ function Expenses(props) {
           selected={enteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
